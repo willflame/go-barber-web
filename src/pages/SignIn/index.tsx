1d66f6e0a12c3305deb49cf50bbb6b1caa926595
Willian Anselmo <willian0506@hotmail.com>
2020-12-26 16:57:01 -0300

    Adicionando e removendo Toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -55,7 +55,11 @@ const SignIn: React.FC = () => {
         }
 
         // Disparar um toast
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
     },
     [signIn, addToast],
