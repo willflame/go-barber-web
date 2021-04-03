11e8fb4f5787c9b4b8289102de9a4c536e62fc2b
Willian Anselmo <willian0506@hotmail.com>
2021-04-03 16:47:52 -0300

    Disponibilidade do mês

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -43,11 +45,12 @@ const AuthProvider: React.FC = ({ children }) => {
       password,
     });
 
-    // console.log(response);
     const { token, user } = response.data;
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
